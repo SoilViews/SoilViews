48c367aff54db5b59e8b43b01cc8ee5447ed8ba8
Aleksander Velchev <a.velchev@ds-bg.com>
2020-02-12 10:33:12 +0200

    Play with events

--- src/components/dashboard/Dashboard.js
+++ src/components/dashboard/Dashboard.js
@@ -1,10 +1,11 @@
-import React from "react";
+import React,{createRef}from "react";
 import { Map, TileLayer, FeatureGroup,Polygon } from "react-leaflet";
 import { compose } from "redux";
 import "leaflet/dist/leaflet.css";
 import { EditControl } from "react-leaflet-draw";
 // import L from "leaflet";
 
+//Hardcoded cordinates of polygons
 const polygon = [
   [42.696295, 23.303643],
   [42.699295, 23.303643],
@@ -13,21 +14,47 @@ const polygon = [
 ]
 
 export class Dashboard extends React.Component {
-  constructor() {
-    super();
-    this.state = {
+
+    //Set location when the map is visualized
+    state = {
       lat: 42.696295,
       lng: 23.303643,
       zoom: 10
-    };
+    }
+    
+  mapRef = createRef()
+
+  handleClick = () => {
+    const map = this.mapRef.current
+    if (map != null) {
+      map.leafletElement.locate()
+      
+    }
+    console.log("CatchHandleClick",Polygon.latlng);
   }
+
+  //TODO
+  handleLocationFound = (e) => {
+    this.setState({
+      hasLocation: true,
+      latlng: e.latlng,
+      
+    }
+ 
+    )
+    console.log("e")
+  }
+
+  
 
   render() {
     const position = [this.state.lat, this.state.lng];
 
     return (
       <div id="map" className="dashboard container">
-        <Map style={{ height: "50vh" }} center={position} zoom={13}>
+        <Map style={{ height: "50vh" }} center={position} zoom={13}
+          onClick={this.handleClick}
+          onLocationfound={this.handleLocationFound}>
           <TileLayer
             attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
             url="http://{s}.tile.osm.org/{z}/{x}/{y}.png"
