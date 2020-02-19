bf657aea803bbb14ec417d2fcb6e01fbc86b703c
Aleksander Velchev <a.velchev@ds-bg.com>
2020-02-19 10:56:01 +0200

    Updates

---
+++ precache-manifest.0dd3a9d46563c0ff20ef765098ec878e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e916e7cee5488b92e84d026f06e21cd8",
+    "url": "/SoilViews/index.html"
+  },
+  {
+    "revision": "a5726b91eb5c39ce287e",
+    "url": "/SoilViews/static/css/2.098bc035.chunk.css"
+  },
+  {
+    "revision": "2cf60f49614f8e6151d3",
+    "url": "/SoilViews/static/css/main.511bf896.chunk.css"
+  },
+  {
+    "revision": "a5726b91eb5c39ce287e",
+    "url": "/SoilViews/static/js/2.82211a18.chunk.js"
+  },
+  {
+    "revision": "2cf60f49614f8e6151d3",
+    "url": "/SoilViews/static/js/main.fe473a0e.chunk.js"
+  },
+  {
+    "revision": "7755171efa56b91fe655",
+    "url": "/SoilViews/static/js/runtime-main.18cd56ee.js"
+  }
+]);
