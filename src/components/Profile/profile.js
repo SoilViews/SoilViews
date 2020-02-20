45377949d838b75ec5c6a62a80aa022ecfb589e8
Aleksander Velchev <a.velchev@ds-bg.com>
2020-02-20 15:34:34 +0200

    added material design and some little fixes

--- src/components/Profile/profile.js
+++ src/components/Profile/profile.js
@@ -6,10 +6,12 @@ const Profile = (props) => {
     return (
         
         <div className="card-action">
-            <h4 className="card-title center-align">Profile</h4>
+        <h4 className="card-title center-align">Profile</h4>
         <p> <span >Username: </span>{props.profile.firstName} </p>
         <p> <span >Last Name: </span>{props.profile.lastName} </p>
         <p> <span>Email: </span>{props.profile.email} </p>
+        <p> <span>City: </span>{props.profile.city} </p>
+        <p> <span>Telephone number: </span>{props.profile.telephone} </p>
   
     </div>
         
--- src/components/layout/Footer.js
+++ src/components/layout/Footer.js
@@ -1,40 +1,75 @@
-import React from 'react';
-import { connect } from 'react-redux'
+import React from "react";
+import { connect } from "react-redux";
 
-const Footer = (props) => {
+const Footer = props => {
+  return (
+    <React.Fragment>
+      <footer class="page-footer orange">
+        <div class="container">
+          <div class="row">
+            <div class="col l6 s12">
+              <h5 class="white-text">Company Bio</h5>
+              <p class="grey-text text-lighten-4">
+                We are a team of college students working on this project like
+                it's our full time job. Any amount would help support and
+                continue development on this project and is greatly appreciated.
+              </p>
+            </div>
+            <div class="col l3 s12">
+              <h5 class="white-text">Settings</h5>
+              <ul>
+                <li>
+                  <a class="white-text" href="#!">
+                    Link 1
+                  </a>
+                </li>
+                <li>
+                  <a class="white-text" href="#!">
+                    Link 2
+                  </a>
+                </li>
+              </ul>
+            </div>
+            <div class="col l3 s12">
+              <h5 class="white-text">Connect</h5>
+              <ul>
+                <li>
+                  <a class="white-text" href="#!">
+                    Link 1
+                  </a>
+                </li>
+                <li>
+                  <a class="white-text" href="#!">
+                    Link 2
+                  </a>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </div>
+        <div class="footer-copyright">
+          <div class="container">
+            Made by{" "}
+            <a
+              class="orange-text text-lighten-3"
+              href="https://soilviews.com/"
+            >
+              SoilViews
+            </a>
+          </div>
+        </div>
+      </footer>
+    </React.Fragment>
+  );
+};
 
+const mapStateToProps = state => {
+  console.log(state);
+  return {
+    projects: state.firestore.ordered.projects,
+    profile: state.firebase.profile,
+    auth: state.firebase.auth
+  };
+};
 
-
-
-    return (
-        <React.Fragment>
-            <footer className="page-footer">
-                <div className="container">
-                    <div className="row">
-                       
-                       
-                    </div>
-                </div>
-                <div className="footer-copyright">
-                    <div className="container">
-                        © 2020 Copyright by Team HAG
-                    </div>
-                </div>
-            </footer>
-        </React.Fragment >
-    )
-}
-
-
-const mapStateToProps = (state) => {
-    console.log(state)
-    return {
-        projects: state.firestore.ordered.projects,
-        profile: state.firebase.profile,
-        auth: state.firebase.auth
-    }
-}
-
-export default connect(
-    (mapStateToProps)
-)(Footer)
+export default connect(mapStateToProps)(Footer);
