0e0c0ba9c925ed8101bce387c7fde13c6d0d5df4
ddachkinov <dimitar.dachkinov@dxc.com>
2020-03-02 12:25:45 +0200

    File upload first steps

--- src/App.js
+++ src/App.js
@@ -12,6 +12,7 @@ import Footer from './components/layout/Footer'
 import Profile from '../src/components/Profile/profile'
 import ContactForm from '../src/components/ContactForm/ContactForm'
 import Documentation from "./components/Help/HelpPage";
+import FileUpload from "./components/FileUpload/index";
 
 
 function App() {
--- src/firebase/index.js
+++ src/firebase/index.js
@@ -9,7 +9,7 @@ const firebaseConfig = {
   authDomain: 'robust-solution-221513.firebaseapp.com',
   databaseURL: 'https://robust-solution-221513.firebaseio.com',
   projectId: 'robust-solution-221513',
-  storageBucket: 'robust-solution-221513.appspot.com',
+  storageBucket: 'gs://robust-solution-221513.appspot.com',
   messagingSenderId: '8658609726',
   appId: '1:8658609726:web:70e9e4e7190b615aa3136f',
   measurementId: 'G-KZCRXQYB7Y'
