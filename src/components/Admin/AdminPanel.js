6ff586d4fbaa4a31dd715d9eb4f254d2ec9dfe92
alvelchev <al.velchevv@gmail.com>
2020-03-28 17:57:43 +0200

    materialize user dataTable

--- src/components/Admin/AdminPanel.js
+++ src/components/Admin/AdminPanel.js
@@ -1,6 +1,14 @@
 import React from "react";
 import { compose } from "redux";
-import {getFirestore } from 'redux-firestore';
+import { getFirestore } from 'redux-firestore';
+import { withStyles, makeStyles } from '@material-ui/core/styles';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableContainer from '@material-ui/core/TableContainer';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Paper from '@material-ui/core/Paper';
 
 
 export class AdminPanel extends React.Component {
@@ -11,72 +19,91 @@ export class AdminPanel extends React.Component {
       UserData: []
     }
 
-}
+  }
 
 
 
-  
+
   componentWillMount() {
     const { UserData } = this.state;
     const firestore = getFirestore();
     firestore.collection("users").get().then(data => {
-  
+
       data.forEach(doc => {
 
         // console.log( "User",doc.data());
-        UserData.push(doc.data());   
-       
-    
+        UserData.push(doc.data());
+
+
       });
       // UserData.map((user) => { return (console.log(user.city));});
-      console.log("User:", UserData); 
+      console.log("User:", UserData);
     });
-    
-  
-  
+
+
+
   }
 
 
 
   render() {
-    const { UserData } = this.state;  
-    
-    return(
-       <div>
-          <button onClick={this.onLoad}>Get specific user data</button>
-          <table>
-          <th>city</th>
-          <th>email</th>
-          <th>firstName</th>
-          <th>initials</th>
-          <th>lastName</th>
-          <th>Actions</th>
-          {UserData.map((user) => {
-               
-               return (
-                 
-                 <tr key={Math.random()}>
-                    <td>{user.city}</td>
-                    <td>{user.email}</td>
-                    <td>{user.firstName}</td>
-                    <td>{user.initials}</td>
-                    <td>{user.lastName}</td>
-                    <td>
-                       <button>Edit</button>
-                       <button>Delete</button>
-                    </td>
-                    
-                 </tr>);}
- )}     
-          <tbody>
-            
-             
-        </tbody>
-      </table>
-       </div>
-  
+    const { UserData } = this.state;
+    const StyledTableCell = withStyles(theme => ({
+      head: {
+        backgroundColor: theme.palette.common.black,
+        color: theme.palette.common.white,
+      },
+      body: {
+        fontSize: 14,
+      },
+    }))(TableCell);
+
+
+    const StyledTableRow = withStyles(theme => ({
+      root: {
+        '&:nth-of-type(odd)': {
+          backgroundColor: theme.palette.background.default,
+        },
+      },
+    }))(TableRow);
+    return (
+      <div className="input-field col s12">
+        <button onClick={this.onLoad}>Get specific user data</button>
+        <TableContainer component={Paper}>
+          <Table aria-label="customized table">
+            <TableHead>
+              <TableRow>
+                <StyledTableCell>city</StyledTableCell>
+                <StyledTableCell>email</StyledTableCell>
+                <StyledTableCell>firstName</StyledTableCell>
+                <StyledTableCell>initials</StyledTableCell>
+                <StyledTableCell>lastName</StyledTableCell>
+                <StyledTableCell>Actions</StyledTableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {UserData.map((user) => {
+                return (
+                  <StyledTableRow key={Math.random()}>
+                    <StyledTableCell>{user.city}</StyledTableCell>
+                    <StyledTableCell>{user.email}</StyledTableCell>
+                    <StyledTableCell>{user.firstName}</StyledTableCell>
+                    <StyledTableCell>{user.initials}</StyledTableCell>
+                    <StyledTableCell>{user.lastName}</StyledTableCell>
+                    <StyledTableCell>
+                      <button class="waves-effect waves-light btn-small">Edit</button>
+                      <button class="waves-effect waves-light btn-small">Delete</button>
+                    </StyledTableCell>
+
+                  </StyledTableRow>);
+              }
+              )}
+            </TableBody>
+          </Table>
+        </TableContainer>
+      </div>
+
     );
   }
-  }
-  export default compose()(AdminPanel)
-  
+}
+export default compose()(AdminPanel)
