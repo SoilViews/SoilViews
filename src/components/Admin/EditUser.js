f28874157aafcc70a8c135c0dae775bac51f1031
alvelchev <al.velchevv@gmail.com>
2020-04-17 21:13:14 +0300

    edit user full form

--- src/components/Admin/EditUser.js
+++ src/components/Admin/EditUser.js
@@ -29,6 +29,11 @@ class EditUser extends Component {
             this.setState({
                 key: '',
                 city: '',
+                email: '',
+                firstName: '',
+                lastName: '',
+                telephone: '',
+
             });
             this.props.history.push(`/AdminPanel`)
         })  
@@ -46,7 +51,15 @@ class EditUser extends Component {
                 const users = doc.data();
                 this.setState({
                     key: doc.id,
-                    city: users.city
+                    city: users.city,
+                    email: users.email,
+                    firstName: users.firstName,
+                    lastName: users.lastName,
+                    telephone: users.telephone,
+                    initials: users.initials
+
+
+
 
                 });
 
@@ -71,16 +84,34 @@ class EditUser extends Component {
             <div class="container">
                 <div class="panel panel-default">
                     <div class="panel-heading">
-                        <h3 class="panel-title">
-                            EDIT USER
-                        </h3>
+                    <h1 class="header center orange-text">Edit User</h1>
                     </div>
                     <div class="panel-body">
-                        <h4><Link to={`/AdminPanel`} class="btn btn-primary">Back to admin panel</Link></h4>
+                        <h4><Link to={`/AdminPanel`} className="btn btn-default search" >Back to admin panel</Link></h4>
                     <form onSubmit={this.onSubmit}>
                         <div class="form-group">
-                            <label for="title">City:</label>
+                            <label for="title">City:</label> <br />
                             <input type="text" class="form-control" name="city" value={this.state.city} onChange={this.onChange} placeholder="Title" />
+                        </div>
+                        <div class="form-group">
+                            <label for="title">Email:</label>  <br />
+                            <input type="text" class="form-control" name="city" value={this.state.email} onChange={this.onChange} placeholder="Title" />
+                        </div>
+                        <div class="form-group">
+                            <label for="title">First name:</label>  <br />
+                            <input type="text" class="form-control" name="city" value={this.state.firstName} onChange={this.onChange} placeholder="Title" />
+                        </div>
+                        <div class="form-group">
+                            <label for="title">Last name:</label>  <br />
+                            <input type="text" class="form-control" name="city" value={this.state.lastName} onChange={this.onChange} placeholder="Title" />
+                        </div>
+                        <div class="form-group">
+                            <label for="title">Telephone:</label>  <br />
+                            <input type="text" class="form-control" name="city" value={this.state.telephone} onChange={this.onChange} placeholder="Title" />
+                        </div>
+                        <div class="form-group">
+                            <label for="title">Initials:</label>  <br />
+                            <input type="text" class="form-control" name="city" value={this.state.initials} onChange={this.onChange} placeholder="Title" />
                         </div>
                         <button type="submit" class="btn btn-success">Submit</button>
                     </form>
