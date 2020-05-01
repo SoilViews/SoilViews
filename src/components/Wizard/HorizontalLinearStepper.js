2809556335e172859127fcc32796c071682f0caa
alvelchev <al.velchevv@gmail.com>
2020-05-01 21:59:06 +0300

    Get state of checkboxes between two steps

--- src/components/Wizard/HorizontalLinearStepper.js
+++ src/components/Wizard/HorizontalLinearStepper.js
@@ -20,12 +20,16 @@ export function FoodCheckbox() {
     setState({ ...state, [event.target.name]: event.target.checked });
     // console.log("You Selected: ", { ...state });
   };
-  SelectedFood({state})
+  SelectedFood({ state });
   return (
     <FormGroup row>
       <FormControlLabel
         control={
-          <Checkbox checked={state.checkedA} onChange={handleChange} name="checkedA" />
+          <Checkbox
+            checked={state.checkedA}
+            onChange={handleChange}
+            name="checkedA"
+          />
         }
         label="Secondary"
       />
@@ -45,11 +49,9 @@ export function FoodCheckbox() {
 }
 
 export function SelectedFood(checked) {
-  console.log(checked)
-  const result = JSON.stringify(checked)
-  return (
-    <div>You selected: {result}</div>
-  );
+  console.log(checked);
+  const result = JSON.stringify(checked);
+  return <div>You selected: {result}</div>;
 }
 
 const useStyles = makeStyles((theme) => ({
@@ -74,49 +76,19 @@ function getSteps() {
   ];
 }
 
-function getStepContent(step) {
-  switch (step) {
-    case 0:
-      return (
-        <div>
-          <h3>Map your crop from satellite</h3>
-          <p>
-            Use satellite imagery to visualise the crop variation within your fields. Furthermore,
-            you can easily create variation maps and prescription files to control the application
-            rate of your fertilizer spreader or sprayer. Zoom into your field by using the search
-            box to find your location! The background map is there to help you find your field and
-            has nothing to do with current satellite imagery.
-          </p>
-        </div>
-      );
-    case 1:
-      return (
-        <ol>
-          <h3>Find your block and chose satellite image</h3>
-          <li>
-            Find the parcel you would like to have a closer look at. Enter the location in the
-            search field at the top left. You can also zoom in and out by using the + and - buttons
-            and navigate by dragging the map to where you want to go.
-          </li>
-          <li>Draw one or more parcels in the background map</li>
-          <li>When you have selected parcels, click on Save</li>
-        </ol>
-      );
-    case 2:
-      return <FoodCheckbox />;
-    case 3:
-      return <SelectedFood />;
-    default:
-      return "Unknown step";
-  }
-}
-
 export default function HorizontalLinearStepper() {
   const classes = useStyles();
   const [activeStep, setActiveStep] = React.useState(0);
   const [skipped, setSkipped] = React.useState(new Set());
   const steps = getSteps();
-
+  const handleChange = (event) => {
+    setState({ ...state, [event.target.name]: event.target.checked });
+    // console.log("You Selected: ", { ...state });
+  };
+  const [state, setState] = React.useState({
+    checkedA: false,
+    checkedB: false,
+  });
   const isStepOptional = (step) => {
     return step === 1;
   };
@@ -140,21 +112,6 @@ export default function HorizontalLinearStepper() {
     setActiveStep((prevActiveStep) => prevActiveStep - 1);
   };
 
-  // const handleSkip = () => {
-  //   if (!isStepOptional(activeStep)) {
-  //     // You probably want to guard against something like this,
-  //     // it should never occur unless someone's actively trying to break something.
-  //     throw new Error("You can't skip a step that isn't optional.");
-  //   }
-
-  //   setActiveStep((prevActiveStep) => prevActiveStep + 1);
-  //   setSkipped((prevSkipped) => {
-  //     const newSkipped = new Set(prevSkipped.values());
-  //     newSkipped.add(activeStep);
-  //     return newSkipped;
-  //   });
-  // };
-
   const handleReset = () => {
     setActiveStep(0);
   };
@@ -164,75 +121,147 @@ export default function HorizontalLinearStepper() {
     var cb = document.getElementsByClassName("leaflet-draw-draw-polygon");
     return !cb[0].dispatchEvent(ee);
   };
-  return (
-    <div className={classes.root}>
-      <Stepper activeStep={activeStep}>
-        {steps.map((label, index) => {
-          const stepProps = {};
-          const labelProps = {};
-          if (isStepOptional(index)) {
-            labelProps.optional = <Typography variant="caption">Optional</Typography>;
-          }
-          if (isStepSkipped(index)) {
-            stepProps.completed = false;
-          }
-          return (
-            <Step key={label} {...stepProps}>
-              <StepLabel {...labelProps}>{label}</StepLabel>
-            </Step>
-          );
-        })}
-      </Stepper>
+  if (activeStep === 0)
+    return (
       <div>
-        {activeStep === steps.length ? (
-          <div>
-            <Typography className={classes.instructions}>
-              All steps completed - you&apos;re finished
-            </Typography>
-            <Button onClick={handleReset} className={classes.button}>
-              Reset
-            </Button>
-          </div>
-        ) : (
-          <div>
-            <Typography className={classes.instructions}>{getStepContent(activeStep)}</Typography>
-            <div>
-              <Button
-                style={{
-                  ...(activeStep === 0 ? { display: "none" } : {}),
-                }}
-                disabled={activeStep === 0}
-                onClick={handleBack}
-                className={classes.button}
-              >
-                Back
-              </Button>
-
-              {isStepOptional(activeStep) && (
-                <Button
-                  variant="contained"
-                  color="primary"
-                  onClick={(e) => {
-                    mapEvent(e);
-                  }}
-                  className={classes.button}
-                >
-                  Draw a polygon
-                </Button>
-              )}
-
-              <Button
-                variant="contained"
-                color="primary"
-                onClick={handleNext}
-                className={classes.button}
-              >
-                {activeStep === steps.length - 1 ? "Finish" : activeStep === 0 ? "Start" : "Next"}
-              </Button>
-            </div>
-          </div>
-        )}
+        <div>
+          <h3>Map your crop from satellite</h3>
+          <p>
+            Use satellite imagery to visualise the crop variation within your
+            fields. Furthermore, you can easily create variation maps and
+            prescription files to control the application rate of your
+            fertilizer spreader or sprayer. Zoom into your field by using the
+            search box to find your location! The background map is there to
+            help you find your field and has nothing to do with current
+            satellite imagery.
+          </p>
+        </div>
+        <Button variant="contained" color="primary" onClick={handleNext}>
+          {activeStep === activeStep.length - 1
+            ? "Finish"
+            : activeStep === 0
+            ? "Start"
+            : "Next"}
+        </Button>
       </div>
-    </div>
-  );
+    );
+  if (activeStep === 1)
+    return (
+      <div>
+        <ol>
+          <h3>Find your block and chose satellite image</h3>
+          <li>
+            Find the parcel you would like to have a closer look at. Enter the
+            location in the search field at the top left. You can also zoom in
+            and out by using the + and - buttons and navigate by dragging the
+            map to where you want to go.
+          </li>
+          <li>Draw one or more parcels in the background map</li>
+          <li>When you have selected parcels, click on Save</li>
+        </ol>
+        <Button variant="contained" color="primary" onClick={handleNext}>
+          {activeStep === activeStep.length - 1
+            ? "Finish"
+            : activeStep === 0
+            ? "Start"
+            : "Next"}
+        </Button>
+        &nbsp; &nbsp;
+        <Button
+          variant="contained"
+          color="primary"
+          onClick={(e) => {
+            mapEvent(e);
+          }}
+        >
+          Draw a polygon
+        </Button>
+        &nbsp; &nbsp;
+        <Button
+          style={{
+            ...(activeStep === 0 ? { display: "none" } : {}),
+          }}
+          disabled={activeStep === 0}
+          onClick={handleBack}
+        >
+          Back
+        </Button>
+      </div>
+    );
+  if (activeStep === 2)
+    return (
+      <div>
+        <div>
+          <h3>Choose your crops type of drawed land</h3>
+          <p>Crops type:</p>
+          <FoodCheckbox handleChange={handleChange} />
+        </div>
+        <Button variant="contained" color="primary" onClick={handleNext}>
+          {activeStep === activeStep.length - 1
+            ? "Finish"
+            : activeStep === 0
+            ? "Start"
+            : "Next"}
+        </Button>
+        <Button
+          style={{
+            ...(activeStep === 0 ? { display: "none" } : {}),
+          }}
+          disabled={activeStep === 0}
+          onClick={handleBack}
+        >
+          Back
+        </Button>
+      </div>
+    );
+  if (activeStep === 3)
+    return (
+      <div>
+        <div>
+          <h3>Selected crops type</h3>
+          <p>Crops type</p>
+          <FoodCheckbox handleChange={handleChange} />
+        </div>
+        <Button variant="contained" color="primary" onClick={handleNext}>
+          {activeStep === activeStep.length - 1
+            ? "Finish"
+            : activeStep === 0
+            ? "Start"
+            : "Next"}
+        </Button>
+        <Button
+          style={{
+            ...(activeStep === 0 ? { display: "none" } : {}),
+          }}
+          disabled={activeStep === 0}
+          onClick={handleBack}
+        >
+          Back
+        </Button>
+      </div>
+    );
+  if (activeStep === 4)
+    return (
+      <div>
+        <div>
+          <h3>Finish</h3>
+          <p>finish</p>
+        </div>
+        <Button variant="contained" color="primary" onClick={handleNext}>
+          {activeStep === 4 ? "Finish" : activeStep === 0 ? "Start" : "Next"}
+        </Button>
+        <Button onClick={handleReset} className={classes.button}>
+          Reset
+        </Button>
+      </div>
+    );
+  if (activeStep === 5)
+    return (
+      <div>
+        <Typography className={classes.instructions}>
+          All steps completed - you&apos;re finished
+        </Typography>
+        <Button className={classes.button}>Thank you!</Button>
+      </div>
+    );
 }
