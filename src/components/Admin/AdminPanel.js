4229756e3bc680e971db7b29e1e856efe8ac8d9d
alvelchev <al.velchevv@gmail.com>
2020-05-06 19:48:56 +0300

    if admin see all orders else only yours

--- src/components/Admin/AdminPanel.js
+++ src/components/Admin/AdminPanel.js
@@ -133,32 +133,46 @@ const mapStateToProps = (state) => {
   };
 };
 
+// export default compose(
+//   connect(mapStateToProps),
+
+//   firestoreConnect((props) => [
+//     { collection: "orders", where: [["userId", "==", props.uid]] },
+//     { collection: "notifications", limit: 5, orderBy: ["time", "desc"] },
+//     { collection: "users" },
+//   ])
+
+//IF ADMIN SEEL ALL ORDERS, ELSE SEE ONLY YOURS
 export default compose(
   connect(mapStateToProps),
-
-  firestoreConnect((props) => [
-    { collection: "orders", where: [["userId", "==", props.uid]] },
-    { collection: "notifications", limit: 5, orderBy: ["time", "desc"] },
-    { collection: "users" },
-  ])
-
-  // export default compose(
-  //   connect(mapStateToProps),
-  //   firestoreConnect((props) => {
-  //     if (!props.profile.role === "Admin") return [{ collection: "orders" }];
-  //     return [
-  //       {
-  //         collection: "orders",
-  //         where: ["authorFirstName", "==", "Alexander"],
-  //       },
-  //       {
-  //         collection: "users",
-  //       },
-  //       {
-  //         collection: "notifications",
-  //         limit: 5,
-  //         orderBy: ["time", "desc"],
-  //       },
-  //     ];
-  //   })
+  firestoreConnect((props) => {
+    if (props.profile.role === "Admin")
+      return [
+        {
+          collection: "orders",
+        },
+        {
+          collection: "users",
+        },
+        {
+          collection: "notifications",
+          limit: 5,
+          orderBy: ["time", "desc"],
+        },
+      ];
+    return [
+      {
+        collection: "orders",
+        where: ["userId", "==", props.uid],
+      },
+      {
+        collection: "users",
+      },
+      {
+        collection: "notifications",
+        limit: 5,
+        orderBy: ["time", "desc"],
+      },
+    ];
+  })
 )(AdminPanel);
