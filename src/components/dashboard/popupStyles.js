e8c6609b2a133601b56d15e161a029e7e1754e7d
al.velchevv@gmail.com <al.velchevv@gmail.com>
2020-06-08 11:46:41 +0300

    .

---
+++ src/components/dashboard/popupStyles.js
@@ -0,0 +1,19 @@
+const popupContent = {
+  height: "200px",
+  marginTop: "30px",
+};
+const popupHead = {
+  fontWeight: "bold",
+  fontSize: "22px",
+};
+
+const popupText = {
+  fontSize: "15px",
+  marginBottom: "20px",
+};
+
+const okText = {
+  fontSize: "15px",
+};
+
+export { popupContent, popupHead, popupText, okText };
