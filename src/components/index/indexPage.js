040200e91cf3abadd21f6d74e21be9f48063e725
Dimitar Dachkinov <ddachkinov@gmail.com>
2020-09-10 19:12:11 +0300

    typo fixed

--- src/components/index/indexPage.js
+++ src/components/index/indexPage.js
@@ -13,8 +13,8 @@ export class indexPage extends React.Component {
         <div className="container">
           <div className="row center">
             <Typography variant="h1" className={styles.orangeText} >{t('welcome')}</Typography>
-            <Typography varian="body1" className={styles.statement} >{t('Get your soil analysis today')}</Typography>
-            <Typography varian="body1" className={styles.statement} >{t('Increase your predictability and crop yields')}</Typography>
+            <Typography variant="body1" className={styles.statement} >{t('Get your soil analysis today')}</Typography>
+            <Typography variant="body1" className={styles.statement} >{t('Increase your predictability and crop yields')}</Typography>
             <Typography variant="body1" className={styles.statement} >{t('we combine soil samples and Copernicus satelite imaging')}</Typography>
           </div>
         </div>
@@ -23,8 +23,8 @@ export class indexPage extends React.Component {
             <div className="row center">
               <Link to="/Dashboard">
                 <h5 className="center btn waves-effect waves-light">
-                {t('Start Here')}
-                  </h5>
+                  {t('Start Here')}
+                </h5>
               </Link>
             </div>
           </div>
