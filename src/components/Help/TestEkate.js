7a8d51a9f51913be870bf10ef33969d8889a96d7
Dimitar Dachkinov <ddachkinov@gmail.com>
2020-10-28 01:20:21 +0200

    for testing

--- src/components/Help/TestEkate.js
+++ src/components/Help/TestEkate.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React from "react";
 import {
   Grid,
   FormControl,
@@ -7,11 +7,11 @@ import {
   Select,
 } from "@material-ui/core";
 
-import styles from "./testEkate.module.css"
-import data from "./testEkate.json"
+import styles from "./testEkate.module.css";
+import data from "./testEkate.json";
 
 const TestEkate = () => {
-//javascript
+  //javascript
   const [province, setProvince] = React.useState("");
   const [mun, setMun] = React.useState("");
   const [land, setLand] = React.useState("");
@@ -33,99 +33,111 @@ const TestEkate = () => {
     setArray(event.target.value);
   };
 
-  //gets the unique Provinces 
-  let uniqieProvince = [...new Set(data.map(item => item.province))]; 
+  //gets the unique Provinces
+  let uniqueProvince = [...new Set(data.map((item) => item.province))].map(
+    (item, key) => {
+      return (
+        <MenuItem key={key} value={item}>
+          {item}
+        </MenuItem>
+      );
+    }
+  );
 
-  //Gets the unique municipalities part of the chosen Province
-  let uniqueMunicipality = [...new Set (data.filter((item) => {
-                        return item.province === province })
-                              .map((item,key) => {
-                        return <MenuItem key={key} value={item.municipality}>{item.municipality}</MenuItem>
-                        }))]; 
+  //filter Municipalities by their province 
+  let filterMun = data
+    .filter((x) => x.province === province)
+    .map(item => {
+      return item.municipality
+    });
 
-    return (
-        <div>
-          {uniqieProvince.map((item, key) => {
-            return <li key={key}>{item}</li>
-          })}
-       <Grid item xs={12} md={6}>
-          <Grid className={styles.center} container spacing={5}>
-            <Grid item xs={12} sm={6} md={6} lg={3}>
-              <FormControl>
-                <InputLabel>Област</InputLabel>
-                <Select
-                  className={styles.selectBox}
-                  labelId=""
-                  id=""
-                  value={province}
-                  onChange={provinceChange}
-                >
-{/* Iterate provinces in the select menu */}
-                  {
-                    uniqieProvince
-                    .map((item,key) => {
-                      return <MenuItem key={key} value={item}>{item}</MenuItem>
-                    })
-                  }
-{/* Iterate provinces in the select menu */}
-                </Select>
-              </FormControl>
-            </Grid>
+//removing the duplicate values from the array of filtered provinces
+  let uniqueMunicipality = filterMun.reduce((acc,item) => {
+    if (!acc.includes(item)){
+      acc.push(item);
+    }
+  return acc
+  },[])
 
-            <Grid item xs={12} sm={6} md={6} lg={3}>
-              <FormControl>
-                <InputLabel>Община</InputLabel>
-                <Select
-                  disabled = {!province}  //Disable until province state is fulfilled
-                  className={styles.selectBox}
-                  labelId=""
-                  id=""
-                  value={mun}
-                  onChange={munChange}
-                >
-                {uniqueMunicipality}
-                </Select>  
-              </FormControl>
-            </Grid>
+  return (
+    <div>
+      <Grid item xs={12} md={6}>
+        <Grid className={styles.center} container spacing={5}>
+          <Grid item xs={12} sm={6} md={6} lg={3}>
+            <FormControl>
+              <InputLabel>Област</InputLabel>
+              <Select
+                className={styles.selectBox}
+                labelId=""
+                id=""
+                value={province}
+                onChange={provinceChange}
+              >
+                <MenuItem disabled>-Select a province-</MenuItem>
+                {/* Iterate provinces in the select menu */}
+                {uniqueProvince}
+                {/* Iterate provinces in the select menu */}
+              </Select>
+            </FormControl>
+          </Grid>
 
-            <Grid item xs={12} sm={6} md={6} lg={3}>
-              <FormControl>
-                <InputLabel>Землище</InputLabel>
-                <Select
-                  className={styles.selectBox}
-                  labelId=""
-                  id=""
-                  value={land}
-                  onChange={landChange}
-                >
-                  {data.map((item,key) => {
-                    return <MenuItem value={key} key={key}>{item.ekatte}</MenuItem>
-                  })}
-                </Select>
-              </FormControl>
-            </Grid>
+          <Grid item xs={12} sm={6} md={6} lg={3}>
+            <FormControl>
+              <InputLabel>Община</InputLabel>
+              <Select
+                disabled={!province} //Disable until province state is fulfilled
+                className={styles.selectBox}
+                labelId=""
+                id=""
+                value={mun}
+                onChange={munChange}
+              >
+                <MenuItem value={uniqueMunicipality}>{uniqueMunicipality}</MenuItem>
+              </Select>
+            </FormControl>
+          </Grid>
 
-            <Grid item xs={12} sm={6} md={6} lg={3}>
-              <FormControl>
-                <InputLabel>Масив</InputLabel>
-                <Select
-                  className={styles.selectBox}
-                  labelId=""
-                  id=""
-                  value={array}
-                  onChange={arrayChange}
-                >
-                  <MenuItem value={10}>101</MenuItem>
-                  <MenuItem value={20}>102</MenuItem>
-                  <MenuItem value={30}>103</MenuItem>
-                </Select>
-              </FormControl>
-            </Grid>
+          <Grid item xs={12} sm={6} md={6} lg={3}>
+            <FormControl>
+              <InputLabel>Землище</InputLabel>
+              <Select
+                className={styles.selectBox}
+                labelId=""
+                id=""
+                value={land}
+                onChange={landChange}
+              >
+                {data.map((item, key) => {
+                  return (
+                    <MenuItem value={key} key={key}>
+                      {item.ekatte}
+                    </MenuItem>
+                  );
+                })}
+              </Select>
+            </FormControl>
+          </Grid>
+
+          <Grid item xs={12} sm={6} md={6} lg={3}>
+            <FormControl>
+              <InputLabel>Масив</InputLabel>
+              <Select
+                className={styles.selectBox}
+                labelId=""
+                id=""
+                value={array}
+                onChange={arrayChange}
+              >
+                <MenuItem value={10}>101</MenuItem>
+                <MenuItem value={20}>102</MenuItem>
+                <MenuItem value={30}>103</MenuItem>
+              </Select>
+            </FormControl>
           </Grid>
         </Grid>
-        </div>
-    );
+      </Grid>
+    </div>
+  );
 };
-
 
 export default TestEkate;
