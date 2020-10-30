029a3b00f250b71f4c5e8f622a77d77368b93ac3
Dimitar Dachkinov <ddachkinov@gmail.com>
2020-10-31 01:03:56 +0200

    Return geo from state|clear select|haskovo added

--- src/components/Wizard/Step2.js
+++ src/components/Wizard/Step2.js
@@ -6,34 +6,97 @@ import {
   InputLabel,
   MenuItem,
   Select,
+  Button,
 } from "@material-ui/core";
 
 import styles from "./Step2.module.css";
 
 // import ekate from "./ekate.json"
 import data from "../../data/ekate.json";
+import haskovo_arrays from "../../data/masivi_haskovo.json";
 
 const Step2 = () => {
   const [province, setProvince] = React.useState("");
   const [mun, setMun] = React.useState("");
   const [settlement, setSettlement] = React.useState("");
   const [array, setArray] = React.useState("");
+  const [findGeo, setFindGeo] = React.useState("");
+  const [ekatte, setEkatte] = React.useState("");
+
+  //Filter GEO by current selection
+  let provinceGeo = data
+    .filter((x) => x.province === province && x.settlement === province)
+    //TODO filter if province !== settlemend return the first closest element + Elin Pelin case
+    .map((item) => {
+      return item.geo;
+    });
+
+  let munGeo = data
+    .filter((x) => x.municipality === mun && x.settlement === mun)
+    .map((item) => {
+      return item.geo;
+    });
+
+  let settlementGeo = data
+    .filter((x) => x.settlement === settlement && x.municipality === mun)
+    .map((item) => {
+      return item.geo;
+    });
+
+  let ekatteGeo = data
+    .filter(
+      (x) =>
+        x.settlement === settlement &&
+        x.municipality === mun &&
+        x.settlement === settlement
+    )
+    .map((item) => {
+      return item.geo;
+    });
+  //USESTATE CHANGE CONSTANTS
 
   const provinceChange = (event) => {
+    setMun();
+    setSettlement();
+    setArray();
+    setEkatte();
+    setFindGeo();
     setProvince(event.target.value);
   };
 
   const munChange = (event) => {
     setMun(event.target.value);
+    setSettlement();
+    setArray();
+    setEkatte();
+    setFindGeo();
+    findGeoCondition();
   };
 
   const settlementChange = (event) => {
     setSettlement(event.target.value);
+
+    setArray();
+    setEkatte();
+    setFindGeo();
+    findGeoCondition();
   };
 
   const arrayChange = (event) => {
     setArray(event.target.value);
+
+    setEkatte();
+    setFindGeo();
+    findGeoCondition();
   };
+
+  const ekatteChange = (event) => {
+    setEkatte(event.target.value);
+    setFindGeo();
+    findGeoCondition();
+  };
+
+  // let currentGeo = provinceGeo ? province : munGeo ? province && mun : settlementGeo ? province && mun && settlement
 
   //gets the unique Provinces using new Set
   let uniqueProvince = [...new Set(data.map((item) => item.province))];
@@ -77,17 +140,32 @@ const Step2 = () => {
       return item.ekatte;
     });
   //filter coordinates by mun and settlement
-  let filterGeo = data
-    .filter((x) => {
-      return (
-        x.municipality === mun &&
-        x.settlement === settlement &&
-        province !== "Хасково"
-      );
-    })
-    .map((item) => {
-      return item.geo;
-    });
+  // let filterGeo = data
+  //   .filter((x) => {
+  //     return (
+  //       x.municipality === mun && x.settlement === settlement
+  //       // && province !== "Хасково"
+  //     );
+  //   })
+  //   .map((item) => {
+  //     return item.geo;
+  //   });
+
+  //Get current GEO and put it into State
+
+  const findGeoCondition = () => {
+    if (province && !mun) {
+      setFindGeo(provinceGeo);
+    } else if (province && mun && !settlement) {
+      setFindGeo(munGeo);
+    } else if (province && mun && settlement) {
+      setFindGeo(settlementGeo);
+    } else if (province && mun && settlement && ekatte) {
+      setFindGeo(ekatteGeo);
+    } else {
+      return null;
+    }
+  };
 
   return (
     <div>
@@ -104,7 +182,8 @@ const Step2 = () => {
         justify="center"
         alignItems="center"
       >
-        <Grid item xs={12} sm={3}>
+        {/* PROVINCE */}
+        <Grid item xs={12} sm={2}>
           <FormControl>
             <InputLabel className={styles.center}>Област</InputLabel>
             <Select
@@ -113,6 +192,7 @@ const Step2 = () => {
               id=""
               value={province}
               onChange={provinceChange}
+              // onBlur={findGeoCondition}
             >
               {/* Iterate unique provinces in the select menu */}
               {uniqueProvince.map((item, key) => {
@@ -125,8 +205,8 @@ const Step2 = () => {
             </Select>
           </FormControl>
         </Grid>
-
-        <Grid item xs={12} sm={3}>
+        {/* MUNICIPALITY */}
+        <Grid item xs={12} sm={2}>
           <FormControl>
             <InputLabel>Община</InputLabel>
             <Select
@@ -148,8 +228,8 @@ const Step2 = () => {
             </Select>
           </FormControl>
         </Grid>
-
-        <Grid item xs={12} sm={3}>
+        {/* SETTLEMENT */}
+        <Grid item xs={12} sm={2}>
           <FormControl>
             <InputLabel>Населено място</InputLabel>
             <Select
@@ -171,26 +251,80 @@ const Step2 = () => {
             </Select>
           </FormControl>
         </Grid>
-
-        <Grid item xs={12} sm={3}>
+        {/* EKATTE - TODO: could only appear if more than 1 result exists, otherwise siply  */}
+        <Grid item xs={12} sm={2}>
           <FormControl>
-            <InputLabel>Масив</InputLabel>
+            <InputLabel>ЕКАТТЕ #</InputLabel>
             <Select
-              className={styles.selectBox}
+              disabled={!settlement} //Disable until province state is selected
+              className={styles.settlementBox}
               labelId=""
               id=""
-              value={array}
-              onChange={arrayChange}
+              value={ekatte}
+              onChange={ekatteChange}
+              defaultValue={"ekatte"}
             >
-              <MenuItem value={10}>101</MenuItem>
-              <MenuItem value={20}>102</MenuItem>
-              <MenuItem value={30}>103</MenuItem>
+              {/* Mapping the reduced list of Municipalities */}
+              {filterEkatte.map((item, key) => {
+                return (
+                  <MenuItem key={key} value={item}>
+                    {item}
+                  </MenuItem>
+                );
+              })}
             </Select>
           </FormControl>
         </Grid>
+        {/* HIDE ARRAY IF NOT "HASKOVO" */}
+        {province === "Хасково" ? (
+          <Grid item xs={12} sm={2}>
+            <FormControl>
+              <InputLabel>Масив</InputLabel>
+              <Select
+                className={styles.selectBox}
+                labelId=""
+                id=""
+                value={array}
+                onChange={arrayChange}
+              >
+                {/* Iterate all arrays from  haskovo_arrays.json */}
+                {haskovo_arrays.map((item, key) => {
+                  return (
+                    <MenuItem key={key} value={item.masiv}>
+                      {item}
+                    </MenuItem>
+                  );
+                })}
+              </Select>
+            </FormControl>
+          </Grid>
+        ) : (
+          ""
+        )}
       </Grid>
-      {settlement ? <Typography>ЕКАТТЕ # {filterEkatte}</Typography> : ""}
+      {/* {settlement ? <Typography>ЕКАТТЕ # {filterEkatte}</Typography> : ""}
       {settlement ? <Typography>Coordinates: {filterGeo}</Typography> : ""}
+      {province && !mun ? (
+        <Typography>Coordinates for current select: {provinceGeo}</Typography>
+      ) : province && mun ? (
+        <Typography>Coordinates for current select: {munGeo}</Typography>
+      ) : province && mun && settlement ? (
+        <Typography>Coordinates for current select: {settlementGeo}</Typography>
+      ) : (
+        ""
+      )} */}
+      {province ? (
+        <Button variant="contained" color="primary" onClick={findGeoCondition}>
+          Find
+        </Button>
+      ) : (
+        ""
+      )}
+      {findGeo ? (
+        <Typography> The current place coordinates are: {findGeo}</Typography>
+      ) : (
+        ""
+      )}
     </div>
   );
 };
